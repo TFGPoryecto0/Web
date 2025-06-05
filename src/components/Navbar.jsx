526d4c878f5d29398d93f13c7fd2c261a58ca743
anabelnc5 <anabelnunezc5@gmail.com>
2025-06-05 18:25:43 +0200

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white p-4 flex justify-between items-center">
       <div className="flex items-center space-x-2">
-        <img src="/logo.png" alt="Game Logo" className="h-10" />
-        <span className="text-xl font-bold">ESCAPE ROOM</span>
+        <img src="src\assets\logos\logo.pnf" alt="Game Logo" className="h-10" />
+        <span className="text-xl font-bold">PROYECTO 0</span>
       </div>
       
       <div className="flex space-x-6">
